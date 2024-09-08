147ad03965c57c09269b497aa6eba05f7e75d16c
unknown <breshna2004@gmail.com>
2024-09-08 20:19:38 +0430

    slove linters error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,10 @@
 // tailwind.config.js
 module.exports = {
-  content: [n
+  content: [
     './src/**/*.{js,jsx,ts,tsx}', // Adjust paths as needed
   ],
   theme: {
     extend: {},
   },
-<<<<<<< HEAD
   plugins: [],
 };
-=======
-  plugins: [],
-};
->>>>>>> a2b5fbdc9d909bddc821c3793c5720fcc1997153
