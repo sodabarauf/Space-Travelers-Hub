3b6ca9588ca14670d17582315080e1c43104ddb5
unknown <breshna2004@gmail.com>
2024-09-08 23:59:59 +0430

    slove linters error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
 import App from './App';
-import store from './redux/store';
 
 // Get the root element from the DOM
 const container = document.getElementById('root');
