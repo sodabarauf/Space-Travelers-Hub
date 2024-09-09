34b0f8144f6c2199889a07f773dc78be074b5f6e
unknown <breshna2004@gmail.com>
2024-09-09 20:12:55 +0430

    add snapshots

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Profile from './components/Profile';
 
-import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
